282204970a1111983876f7005111e9523299901b
arefin_aareef <arefin.aareef@gmail.com>
2022-10-21 02:30:07 +0600

    label added

--- JS/main.js
+++ JS/main.js
@@ -1,43 +1,48 @@
 console.log("Welcome to JS practice");
-// creating variable
-// identifier(name must be unique, can't be same as reserved keywords)
-// (_ $ are accepted only for variable names)
-var variableName = "Welcome to variable";
+// CREATING VARIABLE
+// IDENTIFIER(NAME MUST BE UNIQUE, CAN'T BE SAME AS RESERVED KEYWORDS)
+// (_ $ ARE ACCEPTED ONLY FOR VARIABLE NAMES)
+// OPT 1
+var variableName = "variable creating method 1";
 console.log(variableName);
 
-// let has some scope on strings
-let variableName1 = "Welcome to variable";
+// LET HAS SOME SCOPE ON STRINGS
+let variableName1 = "variable creating method 2";
 console.log(variableName1);
 
-// const value shall/will not change. can't reassign value
-const variableName2 = "Welcome to variable";
+// CONST VALUE SHALL/WILL NOT CHANGE. CAN'T REASSIGN VALUE
+const variableName2 = "variable creating method 3";
 console.log(variableName2);
 
-// re-assigning the value
+// RE-ASSIGNING THE VALUE
 let variableName3 = "Welcome to variable";
-variableName3 = "hello";
+variableName3 = "Reassigning variable value";
 console.log(variableName3);
 
-// string
+// STRING
+console.log("STRING");
 const myName = 'Aareef';
 const myNameF = "Arefin";
 console.log(myNameF);
 console.log(myName);
 
-// backticks
+// BACKTICKS
+console.log("BACKTICKS");
 const backticks = `hello, ${myName}`;
 const check = `${2 + 2}`;
 console.log(backticks);
 console.log(check);
 console.log(typeof check); //data type checking
 
-// numbers
+// NUMBERS
+console.log("NUMBERS");
 const firstNumber = 5;
 const secondNumber = 10;
 const result = firstNumber + secondNumber;
 console.log(result);
 
-// booleans
+// BOOLEANS
+console.log("BOOLEANS");
 const isCool = false;
 console.log(isCool);
 console.log(typeof isCool);
@@ -56,20 +61,21 @@ console.log(x);
 // objects
 // const name = 'Aareef'
 // const age = 29;
-
+console.log("OBJECTS");
 const person = {
     name: 'Aareef',
     age: 29,
 }
-
 console.log(person);
 console.log(typeof person);
 
 // dot notation
+console.log("DOT NOTATION");
 console.log(person.name);
 console.log(typeof person.name);
 
 // array
+console.log("ARRAY");
 const arr = [1, 2, 3, 4];
 console.log(arr);
 console.log(typeof arr);
@@ -79,6 +85,7 @@ console.log(date);
 console.log(typeof date);
 
 // comparison operators
+console.log("COMPARISON OPERATOR");
 const a = 10;
 const b = 10;
 console.log(a >= b);
@@ -92,6 +99,7 @@ console.log(a === b);
 console.log(a !== b);
 
 // lgical operators
+console.log("LOGICAL OPERATORS");
 // and && => both operators need to be true
 console.log(true && false);
 // or || => at least one operator need to be true
@@ -126,8 +134,27 @@ for(let i = 0; i < 10; i++) {
     console.log(i);
 }
 
+// functions
+// block of code which performs a task
+console.log("FUNCTION");
+// a function declaration opt 1
+function square(number) {
+    return number * number;
+}
+const result1 = square(5);
+console.log(result1);
 
+// a function declaration opt 2
+const func = function(number) {
+    return number + number;
+}
+const result2 = func(2);
+console.log(result2);
 
-
-
+// a function declaration opt 3
+const func2 = (number) => {
+    return number + number;
+}
+const result3 = func2(10);
+console.log(result3);
 
