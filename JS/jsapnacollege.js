c95cf931a49d2aa01401e6680d6dcfc9dc65d47b
arefin_aareef <arefin.aareef@gmail.com>
2022-10-23 23:06:55 +0600

    JS FOR LOOP

--- JS/jsapnacollege.js
+++ JS/jsapnacollege.js
@@ -9,10 +9,21 @@
 
 // TAKING USER INPUT
 
-let rollButton = document.querySelector('button');
-rollButton.addEventListener('click', inputMessage);
+// let rollButton = document.querySelector('button');
+// rollButton.addEventListener('click', inputMessage);
 
-function inputMessage() {
-    let name = prompt('Enter name of student');
-    rollButton.textContent = 'Roll No. 1: ' + name;
+// function inputMessage() {
+//     let name = prompt('Enter name of student');
+//     rollButton.textContent = 'Roll No. 1: ' + name;
+// }
+
+// LOOPING
+const n = 5;
+for (let i=1; i<=10; i++) {
+    console.log(n + '*' + i + '=' + n*i )
+}
+
+// REVERSE LOOPING
+for (let i=10; i>0; i--) {
+    console.log(n + '*' + i + '=' + n*i )
 }
