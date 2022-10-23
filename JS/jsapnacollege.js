89fa42bbfb25b65a10da777275f09fe8fd94bead
arefin_aareef <arefin.aareef@gmail.com>
2022-10-24 00:38:39 +0600

    conditional statements

--- JS/jsapnacollege.js
+++ JS/jsapnacollege.js
@@ -18,12 +18,22 @@
 // }
 
 // LOOPING
-const n = 5;
-for (let i=1; i<=10; i++) {
-    console.log(n + '*' + i + '=' + n*i )
-}
+
+// const n = 5;
+// for (let i=1; i<=10; i++) {
+//     console.log(n + '*' + i + '=' + n*i )
+// }
 
 // REVERSE LOOPING
-for (let i=10; i>0; i--) {
-    console.log(n + '*' + i + '=' + n*i )
-}
+
+// for (let i=10; i>0; i--) {
+//     console.log(n + '*' + i + '=' + n*i )
+// }
+
+// CONDITIONAL STATEMENT
+var time = prompt("Hey what's the time");
+        if(time>7 && time<17) {
+            alert("Good morning");
+        } else  {
+            alert("Good night");
+        }
