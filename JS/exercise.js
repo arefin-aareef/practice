8964a972b19ee6c35121fa2c2cf45aba8bac2819
arefin_aareef <arefin.aareef@gmail.com>
2022-10-28 02:44:29 +0600

    extending class

--- JS/exercise.js
+++ JS/exercise.js
@@ -42,32 +42,64 @@
 // CLASS
 // DECLARING
 
-class Product {
-    constructor(itemName, price, discount, productCode) {
-        this.itemName = itemName;
-        this.price = price;
-        this.discount = discount;
-        this.productCode = productCode;
-    }
-}
-let pencil = new Product('Pencil', 20, 2, 'P10');
+// class Product {
+//     constructor(itemName, price, discount, productCode) {
+//         this.itemName = itemName;
+//         this.price = price;
+//         this.discount = discount;
+//         this.productCode = productCode;
+//     }
+// }
+// let pencil = new Product('Pencil', 20, 2, 'P10');
+
+
 
 // USING EXPRESSIION
-const Product1 = class Product {
-    constructor(itemName, price, discount, productCode) {
+// const Product1 = class Product {
+//     constructor(itemName, price, discount, productCode) {
+//         this.itemName = itemName;
+//         this.price = price;
+//         this.discount = discount;
+//         this.productCode = productCode;
+//     }
+//     // GETTER SETTER
+//     get getDiscountValue() {
+//         return this.discount;
+//     }
+//     set setDiscountValue(value) {
+//         this.discount = value;
+//     }
+//     // METHOD DEFINITION
+//     discountValue() {
+//         return this.discount * this.price / 100;
+//     }
+
+// }
+
+// let chair = new Product1('Chair', 500, 15, 'C10');
+
+
+class Product {
+    constructor(itemName) {
         this.itemName = itemName;
-        this.price = price;
-        this.discount = discount;
-        this.productCode = productCode;
     }
-    get getDiscountValue() {
-        return this.discount;
+
+    getItemName() {
+        return this.itemName + ' is a product';
     }
-    set setDiscountValue(value) {
-        this.discount = value;
+}
+// EXTENDING CLASS
+
+class Furniture extends Product {
+    constructor(itemName) {
+        super(itemName);
+    }
+
+    getItemName() {
+        return this.itemName + ' is a furniture';
     }
 }
 
-let chair = new Product1('Chair', 500, 15, 'C10');
-
+let pencil = new Product('Pencil');
+let chair = new Furniture('Chair');
 
