3a3779372c34a6395867547169c5bee3b805ce6c
arefin_aareef <arefin.aareef@gmail.com>
2022-11-06 23:51:18 +0600

    this keyword

--- JS/input.js
+++ JS/input.js
@@ -1,7 +1,17 @@
-"use strict";
+// "use strict";
 
-const ps = require("prompt-sync");
-const prompt = ps();
+// const ps = require("prompt-sync");
+// const prompt = ps();
 
-let name = prompt("Enter your name : ");
-console.log(`Hello ${name}`);
+// let name = prompt("Enter your name : ");
+// console.log(`Hello ${name}`);
+
+// EXERCISE FOR OBJECTS
+
+let square = {
+    side : 5,
+    get area () {
+        return this.side * this.side
+    }
+}
+console.log(square.area)
